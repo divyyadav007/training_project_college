6678d75e2298fdf0b345e493e873e33b316cb6e5
divyyadav007 <yadavdivy007@gmail.com>
2026-04-10 13:53:09 -0700

    some codes

--- shops/models.py
+++ shops/models.py
@@ -10,13 +10,14 @@ class Contact(models.Model):
     def __str__(self):
         return self.name
 
-# class Product(models.Model):
-#     name = models.CharField(max_length = 100)
-#     description = models.TextField()
-#     price = models.DecimalField(max_digit =  10 , decimal_places =2)
-#     category = models.CharField(max_length = 50, choices =[('Milk', 'Milk'), ('Ghee', 'Ghee'), ('Curd', 'Curd')])
-#     image = models.Imagefield(upload_to='products/')
-#     stock_quantity = models.IntegerField(default = 0)
+class Product(models.Model):
+    name = models.CharField(max_length = 100)
+    description = models.TextField()
+    price = models.DecimalField(max_digit =  10 , decimal_places =2)
+    category = models.CharField(max_length = 50, choices =[('Milk', 'Milk'), ('Ghee', 'Ghee'), ('Curd', 'Curd')])
+    image = models.Imagefield(upload_to='products/')
+    stock_quantity = models.IntegerField(default = 0)
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self): 
+        return self.name
+
